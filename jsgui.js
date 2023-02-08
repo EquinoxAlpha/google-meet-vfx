e5d4c41dbbfa9161f4259d2f0046b7c39eb0201a
EquinoxAlpha <93602271+EquinoxAlpha@users.noreply.github.com>
2023-02-08 19:44:51 +0000

    new version of this
    the old one looked like stage 5 cancer

--- jsgui.js
+++ jsgui.js
@@ -1,137 +1,235 @@
 // jsgui
 // it's like ImGui but worse
+// except it's not an immediate mode gui library lol
+// it looks similar
 // no idea if a project with the same name exists and I don't care
 
-var jsgui = {
-    currentOperation: null,
-    frame: null,
-    last: null,
-    nextIsInline: false
-}
-
-function drawText(ctx, text, x, y) {
-    var metrics = ctx.measureText(text)
-    var width = metrics.width
-    var height = metrics.actualBoundingBoxAscent
-    ctx.fillText(text, x - width / 2, y + height / 2)
-}
-
-function drawRect(ctx, x, y, w, h) {
-    ctx.fillRect(x - w / 2, y - h / 2, w, h)
-}
-
-function dragElement(elmnt, header) {   
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    header.onmousedown = dragMouseDown;
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
+class JsGui {
+    constructor() {
+        this.windows = [];
+        this.BORDER_COLOR = "1px solid #6D6D7F";
     }
 
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    createWindow(title, x, y, width, height) {
+        const window = {
+            title,
+            x,
+            y,
+            width,
+            height,
+            dragging: false,
+            minimized: false,
+            widgets: [],
+            container: document.createElement("div"),
+            widgetContainer: document.createElement("div")
+        };
+        this.windows.push(window);
+        return window;
     }
 
-    function closeDragElement() {
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-}
-
-function beginWindow(name) {
-    if (jsgui.currentOperation != null) {
-        console.error("Error: beginWindow: endWindow not called before beginning a new one!")
+    createLabel(window, text) {
+        const element = document.createElement("p");
+        element.textContent = text;
+        window.widgetContainer.appendChild(element);
+        const widget = { type: "label", text: text, element: element };
+        return widget;
     }
 
-    jsgui.frame = document.createElement("div")
-    jsgui.frame.style = "position: absolute; left: 50px; top: 50px; background-color: #202020; color: #ffffff; padding: 0px; font-family:monospace;"
-    var elements = document.getElementsByTagName("*");
-    var highest_index = 0;
+    createButton(window, text, onClick) {
+        const element = document.createElement("button");
+        element.textContent = text;
+        element.style.fontFamily = "monospace";
+        element.style.fontWeight = "400";
+        element.style.color = "#E9E9E9";
+        element.style.backgroundColor = "transparent";
+        element.style.border = this.BORDER_COLOR;
+        element.style.cursor = "pointer";
+        element.addEventListener("click", onClick);
+        window.widgetContainer.appendChild(element);
+        const widget = { type: "label", text: text, element: element };
+        return widget;
+    }
 
-    for (var i = 0; i < elements.length - 1; i++) {
-        if (parseInt(elements[i].style.zIndex) > highest_index) {
-            highest_index = parseInt(elements[i].style.zIndex)
+    createImage(window, source, width = "auto", height = "auto") {
+        const element = document.createElement("img")
+        element.src = source;
+        if (width != "auto") {
+            if (width === "fill") {
+                element.style.maxWidth = "100%"
+            } else {
+                element.width = width;
+            }
         }
+        if (height != "auto") {
+            if (height === "fill") {
+                element.style.maxHeight = "100%"
+            } else {
+                element.height = height;
+            }
+        }
+        window.widgetContainer.appendChild(element);
+        const widget = { type: "image", source: source, width: width, height: height, element: element };
+        return widget;
     }
-    jsgui.frame.style.zIndex = highest_index + 1
 
-    jsgui.currentOperation = document.createElement("div")
-    jsgui.currentOperation.style = "background-color: #202020; color: #ffffff; padding: 10px"
-
-    var header = document.createElement("div")
-    header.style = "background-color: #064c88; color: #ffffff; padding: 10px; font-family:monospace;"
-    header.innerHTML = name
-    dragElement(jsgui.frame, header)
-    jsgui.frame.appendChild(header)
-    jsgui.frame.appendChild(jsgui.currentOperation)
-}
-
-function inlineCheck(element) {
-    if (jsgui.nextIsInline) {
-        element.style.display = "inline"
-        jsgui.nextIsInline = false
+    createTextInput(window, placeholder, onChange, text = "", width = 100) {
+        const element = document.createElement("input");
+        element.value = text;
+        element.type = "text";
+        element.placeholder = placeholder;
+        element.style.border = this.BORDER_COLOR;
+        element.style.boxSizing = "border-box";
+        element.style.backgroundColor = "transparent";
+        element.style.color = "#E9E9E9";
+        element.style.marginBottom = "2px";
+        element.style.outline = "none";
+        element.addEventListener("input", onChange);
+        window.widgetContainer.appendChild(element);
+        const widget = { type: "input", input: "text", placeholder: placeholder, width: width, onChange: onChange, element: element };
+        return widget;
     }
-    return element
-}
 
-//Adds some text to the current window.
-function label(text) {
-    var label = inlineCheck(document.createElement("p"))
-    label.innerHTML = text
-    jsgui.currentOperation.appendChild(label)
-    jsgui.last = label
-    return label
-}
-
-function textArea(id) {
-    var area = inlineCheck(document.createElement("textarea"))
-    area.id = id
-    jsgui.currentOperation.appendChild(area)
-    jsgui.last = area
-    return area
-}
-
-function checkbox(id) {
-    var box = inlineCheck(document.createElement("input"))
-    box.type = "checkbox"
-    box.id = id
-    jsgui.currentOperation.appendChild(box)
-    jsgui.last = box
-    return checkbox
-} 
-
-function lineBreak() {
-    jsgui.currentOperation.appendChild(document.createElement("p"))
-}
-
-function sameLine() {
-    jsgui.nextIsInline = true
-}
-
-//Allows you to add a custom HTML5 element.
-function customElement(element) {
-    jsgui.currentOperation.appendChild(inlineCheck(element))
-    jsgui.last = element
-}
-
-function endWindow() {
-    if (jsgui.currentOperation == null) {
-        console.error("Error: endWindow: not creating a window")
+    createContainer(window) {
+        const element = document.createElement("div");
+        window.widgetContainer.appendChild(element)
+        const widget = { type: "container", widgets: [], widgetContainer: element, element: element };
+        return widget;
     }
-    
-    document.body.appendChild(jsgui.frame)
-    jsgui.currentOperation = null;
-    jsgui.frame = null;
-    jsgui.last = null
+
+    createNewLine(window, amount = 1) {
+        if (amount == 1) {
+            const element = document.createElement("p");
+            window.widgetContainer.appendChild(element)
+            const widget = { type: "newline", element: element };
+            return widget;
+        }
+        const widgets = [];
+        for (let i = 0; i < amount; i++) {
+            const element = document.createElement("p");
+            window.widgetContainer.appendChild(element)
+            const widget = { type: "newline", element: element };
+            widgets.push(widget);
+        }
+        return widgets;
+    }
+
+    createLineSeparator(window, text = null) {
+        const element = document.createElement("div");
+        element.style.maxWidth = "100%"
+        element.style.borderBottom = this.BORDER_COLOR;
+        element.textContent = text;
+        window.widgetContainer.appendChild(element)
+        const widget = { type: "lineSeparator", text: text, element: element };
+        return widget;
+    }
+
+    endWindow(window) {
+        this.renderWindowHeader(window);
+        window.container.appendChild(window.widgetContainer);
+        window.container.style.position = "absolute";
+        window.container.style.left = `${window.x}px`;
+        window.container.style.top = `${window.y}px`;
+        window.container.style.width = `${window.width}px`;
+        window.container.style.height = `${window.height}px`;
+        window.container.style.color = "#E9E9E9";
+        window.container.style.fontFamily = "monospace";
+        window.widgetContainer.style.backgroundColor = "#0F0F0FF0";
+        window.widgetContainer.style.padding = "10px";
+        window.widgetContainer.style.boxSizing = "border-box";
+        window.widgetContainer.style.border = this.BORDER_COLOR;
+        window.widgetContainer.style.borderTop = "none";
+        window.widgetContainer.style.width = `${window.width}px`;
+        document.body.appendChild(window.container);
+    }
+
+    renderWindowHeader(window) {
+        // Create the container element
+        const container = document.createElement("div");
+        container.style.backgroundColor = "#294A7A";
+        container.style.padding = "10px";
+        container.style.borderLeft = this.BORDER_COLOR;
+        container.style.borderRight = this.BORDER_COLOR;
+        container.style.borderTop = this.BORDER_COLOR;
+        container.style.boxSizing = "border-box";
+        container.style.cursor = "move";
+        container.addEventListener("mousedown", event => {
+            window.dragging = true;
+            window.dragOffsetX = event.clientX - window.x;
+            window.dragOffsetY = event.clientY - window.y;
+        });
+        window.container.appendChild(container);
+
+        // Create the title bar element
+        const titleBar = document.createElement("div");
+        titleBar.style.display = "flex";
+        titleBar.style.alignItems = "center";
+        titleBar.style.justifyContent = "space-between";
+        titleBar.textContent = window.title;
+        titleBar.style.userSelect = "none";
+        container.appendChild(titleBar);
+
+        const buttons = document.createElement("div");
+
+        // Create the close button element
+        const closeButton = document.createElement("button");
+        closeButton.textContent = "X";
+        closeButton.style.float = "right";
+        closeButton.style.padding = "0";
+        closeButton.style.marginLeft = "15px";
+        closeButton.style.border = "none";
+        closeButton.style.backgroundColor = "transparent";
+        closeButton.style.color = "white";
+        closeButton.style.cursor = "pointer";
+        closeButton.addEventListener("click", () => {
+            // Remove the window from the list of windows
+            const index = this.windows.indexOf(window);
+            if (index >= 0) {
+                this.windows.splice(index, 1);
+            }
+            // Remove the window's container from the DOM
+            window.container.remove();
+        });
+        buttons.appendChild(closeButton);
+
+        const minimizeButton = document.createElement("button");
+        minimizeButton.textContent = "-";
+        minimizeButton.style.padding = "0";
+        minimizeButton.style.margin = "0";
+        minimizeButton.style.border = "none";
+        minimizeButton.style.backgroundColor = "transparent";
+        minimizeButton.style.color = "white";
+        minimizeButton.style.cursor = "pointer";
+        minimizeButton.addEventListener("click", () => {
+            window.minimized = !window.minimized;
+            if (window.minimized) {
+                window.widgetContainer.hidden = true;
+                container.style.borderBottom = this.BORDER_COLOR;
+                minimizeButton.textContent = "+";
+            } else {
+                window.widgetContainer.hidden = false;
+                container.style.borderBottom = "none";
+                minimizeButton.textContent = "-";
+            }
+        });
+        buttons.appendChild(minimizeButton);
+
+        titleBar.appendChild(buttons);
+
+        // Add mousemove and mouseup event listeners for dragging
+        document.addEventListener("mousemove", event => {
+            if (window.dragging) {
+                window.x = event.clientX - window.dragOffsetX;
+                window.y = event.clientY - window.dragOffsetY;
+                window.container.style.left = `${window.x}px`;
+                window.container.style.top = `${window.y}px`;
+            }
+        });
+        document.addEventListener("mouseup", () => {
+            window.dragging = false;
+        });
+    }
+
+    removeWidget(widget) {
+        widget.element.parentElement.removeChild(widget.element);
+    }
 }
